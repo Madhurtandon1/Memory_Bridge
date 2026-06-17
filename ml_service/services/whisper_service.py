cd39254b4a3a326bb2f96645cd0db814639fca4b
Madhur Tandon <madhurrtandon123@gmail.com>
2026-06-17 12:05:10 +0530

    Add lazy loading for whisper model

--- ml_service/services/whisper_service.py
+++ ml_service/services/whisper_service.py
@@ -1,8 +1,30 @@
+# import whisper
+
+# model = whisper.load_model("tiny")
+
+# def transcribe_audio(audio_path: str):
+
+#     result = model.transcribe(audio_path)
+
+#     return result["text"]
+
+
 import whisper
 
-model = whisper.load_model("tiny")
+_model = None
+
+def get_model():
+    global _model
+
+    if _model is None:
+        print("Loading Whisper Tiny Model...")
+        _model = whisper.load_model("tiny")
+
+    return _model
 
 def transcribe_audio(audio_path: str):
+
+    model = get_model()
 
     result = model.transcribe(audio_path)
 
